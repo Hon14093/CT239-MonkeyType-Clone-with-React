3b192bb49465857f021e58d318a34b9514a59add
Mhq14093 <mhq14903@gmail.com>
2023-12-31 23:48:39 +0700

    ops: Update config bar

--- src/Components/MTmain.js
+++ src/Components/MTmain.js
@@ -5,60 +5,78 @@ class MTmain extends Component {
         return (
         <>
         {/* config bar */}
-        <section className='flex justify-center items-center mx-12 rounded-lg text-chaosTxt bg-chaosSecond h-9 gap-3 text-xs'>
+        <section className='grid grid-flow-col justify-around text-chaosTxt h-10 gap-3 text-xs'>
+            <div className='flex items-center px-4 gap-3 justify-around bg-chaosSecond rounded-lg'>
+                <article className='flex gap-4'>
+                    <button id='myButton'>
+                        <div className='Ani duration-400'>
+                            <i className='fa-solid fa-at mr-2'></i>
+                            punctuation
+                        </div>
+                    </button>
+                    <button>
+                        <div className='Ani duration-400'>
+                            <i className='fa-solid fa-hashtag mr-2'></i>
+                            numbers
+                        </div>
+                    </button>
+                </article>
 
-            <article className='flex gap-4'>
-                <button id='myButton'>
-                    <div className='Ani duration-400'>
-                        <i className='fa-solid fa-at mr-2'></i>
-                        punctuation
-                    </div>
-                </button>
-                <button>
-                    <div className='Ani duration-400'>
-                        <i className='fa-solid fa-hashtag mr-2'></i>
-                        numbers
-                    </div>
-                </button>
-            </article>
+                <div className='w-0.5 h-6 border-2 border-chaosTxt rounded-lg'></div>
 
-            <div className=' h-3/5 border-2 border-chaosTxt rounded-lg'></div>
+                <article className='flex gap-4'>
+                    <button id='myButton'>
+                        <div className='Ani duration-400'>
+                            <i className='fa-solid fa-clock mr-2'></i>
+                            time
+                        </div>
+                    </button>
+                    <button>
+                        <div className='Ani duration-400'>
+                            <i className='fa-solid fa-a mr-2'></i>
+                            words
+                        </div>
+                    </button>
+                    <button>
+                        <div className='Ani duration-400'>
+                            <i className='fa-solid fa-quote-left mr-2'></i>
+                            quote
+                        </div>
+                    </button>
+                    <button>
+                        <div className='Ani duration-400'>
+                            <i className='fas fa-fw fa-mountain mr-2'></i>
+                            zen
+                        </div>
+                    </button>
+                    <button>
+                        <div className='Ani duration-400'>
+                            <i className='fa-solid fa-wrench mr-2'></i>
+                            custom
+                        </div>
+                    </button>
+                </article>
 
-            <article className='flex gap-4'>
-                <button id='myButton'>
-                    <div className='Ani duration-400'>
-                        <i className='fa-solid fa-clock mr-2'></i>
-                        time
-                    </div>
-                </button>
-                <button>
-                    <div className='Ani duration-400'>
-                        <i className='fa-solid fa-a mr-2'></i>
-                        words
-                    </div>
-                </button>
-                <button>
-                    <div className='Ani duration-400'>
-                        <i className='fa-solid fa-quote-left mr-2'></i>
-                        quote
-                    </div>
-                </button>
-                <button>
-                    <div className='Ani duration-400'>
-                        <i className='fa-solid fa-fan mr-2'></i>
-                        zen
-                    </div>
-                </button>
-                <button>
-                    <div className='Ani duration-400'>
-                        <i className='fa-solid fa-wrench mr-2'></i>
-                        custom
-                    </div>
-                </button>
-            </article>
-
-            <div className=' h-3/5 border-2 border-chaosTxt rounded-lg'></div>
-            
+                <div className='w-0.5 h-6 border-2 border-chaosTxt rounded-lg'></div>
+                
+                <section className='flex gap-4'>
+                    <button className='Ani duration-400'>
+                        10
+                    </button>
+                    <button className='Ani duration-400'>
+                        25
+                    </button>
+                    <button className='Ani duration-400'>
+                        50
+                    </button>
+                    <button className='Ani duration-400'>
+                        100
+                    </button>
+                    <button className='Ani duration-400'>
+                        <i className='fa-solid fa-screwdriver-wrench'></i>
+                    </button>
+                </section>
+            </div>
             
         </section>
 
